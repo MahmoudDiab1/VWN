d9445e011b2761156d8df7af082c2e76474016fa
Mahmoud Diab <mahmoud.mohameddiab@vodafone.com>
2022-07-06 22:31:13 +0200

    feat(UI-XTextField): setup XTextField custom view.

--- VWN/Common/XTextField/XTextField.swift
+++ VWN/Common/XTextField/XTextField.swift
@@ -8,25 +8,37 @@
 import UIKit
 
 class XTextField: UIView {
-
+    
+    // MARK: Outlets -
     @IBOutlet var uiContentView: UIView!
     @IBOutlet weak var text: UITextField!
+    
+    // MARK: Initializers -
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSub()
+    }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         initSub()
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initSub()
-    }
-    private func initSub(){
-//        translatesAutoresizingMaskIntoConstraints = false
-        Bundle.main.loadNibNamed(R.nib.xTextField.name, owner: self, options: nil)
+    // MARK: Functions -
+    private func initSub() {
+        uiContentView = loadViewFromNib()
+        uiContentView.frame = bounds
+        uiContentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        uiContentView.translatesAutoresizingMaskIntoConstraints = true
+        uiContentView.backgroundColor = .clear
         addSubview(uiContentView)
-            uiContentView.frame = self.bounds
-      
     }
-
+    
+    private func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of:self))
+        let nib = UINib(nibName: R.nib.xTextField.name, bundle: bundle)
+        let nibView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        return nibView
+    }
+    
 }
